4ea0af872a7cd55c7d819e10841278869af78753
Neil <mr.neil.j@gmail.com>
2018-08-05 21:16:23 -0700

    First runthrough, hopefully this works.....

--- projects/johnsnei/dominion/randomTestAdventurer.c
+++ projects/johnsnei/dominion/randomTestAdventurer.c
@@ -16,18 +16,18 @@ int adventureCardTest(struct gameState *pre, struct gameState *post, int player,
 	int preDrawnTreasures 	= 0;
 	int postDrawnTreasures 	= 0;
 	int preHandCount = pre.handCount[player];
-	int postHandCount = post.handCount[player];
+	int postHandCount = post->handCount[player];
 
 	int n;
 
-	for(n = 0; n < pre.handCount[player]; n++){
-		if(pre.hand[player][n] == copper || pre.hand[player][n] == silver || pre.hand[player][n] == gold){
+	for(n = 0; n < pre->handCount[player]; n++){
+		if(pre->hand[player][n] == copper || pre->hand[player][n] == silver || pre->hand[player][n] == gold){
 			preDrawnTreasures++;
 		}
 	}
 
-	for(n = 0; n < pre.handCount[player]; n++){
-		if(post.hand[player][n] == copper || post.hand[player][n] == silver || post.hand[player][n] == gold){
+	for(n = 0; n < pre->handCount[player]; n++){
+		if(post->hand[player][n] == copper || post->hand[player][n] == silver || post->hand[player][n] == gold){
 			postDrawnTreasures++;
 		}
 	}
